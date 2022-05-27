7af0df53ad1cec7808b0128d2e5b8ed4bc3ee60b
SergioGord <gsricco@yandex.ru>
2022-05-27 19:21:48 +0300

    change package.json and index.tsx (react18->17)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
-    const [value, setValue] = useState<number>(0)
+    const [value, setValue] = useState<number>(0);
+
+
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue');
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString);
+            setValue(newValue);
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value));
+    }, [value])
+
     const incHandler = () => {
         setValue(value + 1);
+        // setToLocalStorageHandler();
     }
 
-    const setToLocalStorageHandler = () => {
+    /*const setToLocalStorageHandler = () => {
         localStorage.setItem('counterValue', JSON.stringify(value));
         localStorage.setItem('counterValue+1', JSON.stringify(value + 1));
     }
@@ -26,6 +41,8 @@ function App() {
     const removeItemLocalStorageHandler = () => {
         localStorage.removeItem('counterValue+1');
     }
+*/
+    //sessionStorage. - тоже самое но время жизни - пока не закрыл вкладку
 
     return (
         <div className="App">
@@ -33,10 +50,10 @@ function App() {
             <hr/>
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            {/*            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
             <button onClick={getFromLocalHandler}>getFromLocalStorage</button>
             <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-            <button onClick={removeItemLocalStorageHandler}>removeItemLocalStorage</button>
+            <button onClick={removeItemLocalStorageHandler}>removeItemLocalStorage</button>*/}
         </div>
     );
 }
